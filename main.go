3f36321ab500cd8c0b41d6f4077eeb06430e1680
zawmoewin120298-collab <zawmoewin120298@gmail.com>
2026-01-05 23:30:34 +0700

    Initialize main.go with a simple HTTP server

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+ 
+import (
+    "fmt"
+    "net/http"
+    "os"
+)
+ 
+func main() {
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+ 
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, World!")
+    })
+ 
+    http.ListenAndServe(":"+port, nil)
+}
